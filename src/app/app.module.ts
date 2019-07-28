e2a33a4cdf4f3d885b8a1461fe6569f471759a7a
Francisco Javier Álvarez Sánchez <francisco.alvarez@kairosds.com>
2019-07-28 18:34:22 +0200

    feat(): add google maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AgmCoreModule } from '@agm/core';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAjoPi7YfA22pIjTfKPC0PMHo-AlvXEhbI'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
