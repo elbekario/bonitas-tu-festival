266de112c671097889ad176c9fdc98f58dc0edc2
Francisco Javier Álvarez Sánchez <francisco.alvarez@kairosds.com>
2020-07-14 11:30:03 +0200

    feat(): refactor app.component.html; schedule coming soon

--- src/app/2019/app.component.ts
+++ src/app/2019/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: '../main.component.html',
   styleUrls: ['../app.component.css'],
 })
 export class App2019Component {
   title = 'bonitas-tu-festival';
   lat = 51.678418;
   lng = 7.809007;
+  background = '../assets/FondoBonitasTu.png';
+  logo = '../assets/BonitasTuLogo2019.png';
+  get video() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      'https://www.youtube.com/embed/O4zfnY1jp4w'
+    );
+  }
+  date = '2 de ago - 4 de ago';
+  schedule = '../assets/BonitasTuHorario.png';
+
+  constructor(private readonly sanitizer: DomSanitizer) {}
 }
--- src/app/2020/app.component.ts
+++ src/app/2020/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: '../main.component.html',
   styleUrls: ['../app.component.css'],
 })
 export class App2020Component {
   title = 'bonitas-tu-festival';
   lat = 51.678418;
   lng = 7.809007;
+  background = '../assets/Fondo2020.png';
+  logo = '../assets/BonitasTuLogo2020.png';
+  get video() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      'https://www.youtube.com/embed/BvQ7vIguDXk'
+    );
+  }
+  date = '24 de julio - 26 de julio';
+  schedule = '../assets/comingsoon.png';
+
+  constructor(private readonly sanitizer: DomSanitizer) {}
 }
